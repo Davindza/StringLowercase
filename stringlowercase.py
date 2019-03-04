bea44627bca4b44b80089bac44f468fc0f95c80c
David <daviderror404@gmail.com>
2019-03-04 16:06:08 +0100

    first commit

---
+++ stringlowercase.py
@@ -0,0 +1,8 @@
+string = "Today Is A BeautiFul DAY"
+print(string.lower())
+
+string = "today is a beautiful day"
+print(string.lower())
+
+string = "TODAY IS A BEAUTIFUL DAY"
+print(string.lower())
